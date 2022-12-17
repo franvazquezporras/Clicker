8a2d61657bccd3dea4bcc26ca95fa186e395054a
Zetaka <franvazquez1993@gmail.com>
2022-12-17 19:21:44 +0100

    OptionsMenu
    Control de guardado de parámetros de opciones y cancelación de modificaciones de los mismos

--- Clicker/Assets/Script/MenuControll.cs
+++ Clicker/Assets/Script/MenuControll.cs
@@ -7,29 +7,40 @@ using UnityEngine.Audio;
 public class MenuControll : MonoBehaviour
 {
 
-    //Variables
-    [Header("UI Component")]
-    [SerializeField] private AudioMixer audioMixer;
+    //Variables    
+    [Header("Resolution")]
     [SerializeField] private Dropdown resolutionDropdown;
     Resolution[] resolutions;
-    [SerializeField] private Image brightnessPanel;
+    [SerializeField] private Toggle fullscreenCheck;
 
+    [Header("Quality")]
+    [SerializeField] private Dropdown qualityDropdown;
+    
+    [Header("Brightness")]
+    [SerializeField] private Image brightnessPanel;
+    [SerializeField] private Slider brightnessSlider;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider masterVolume;
+    [SerializeField] private Slider musicVolume;
+    [SerializeField] private Slider soundVolume;
 
     /*********************************************************************************************************************************/
     /*Funcion: Start                                                                                                                 */
     /*Desarrollador: Vazquez                                                                                                         */
-    /*Descripción: Obtiene las resoluciones posibles de la pantalla                                                                  */
+    /*Descripción: Obtiene las resoluciones posibles de la pantalla y el brillo guardado                                             */
     /*********************************************************************************************************************************/
     private void Start()
     {
         resolutions = Screen.resolutions;
-
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
-
         int currentResolutionIndex = 0;
+        
         for (int i = 0; i < resolutions.Length; i++)
         {
+            
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
 
@@ -41,7 +52,8 @@ public class MenuControll : MonoBehaviour
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-    }
+        LoadSetting();
+    }   
 
     /*********************************************************************************************************************************/
     /*Funcion: SetResolution                                                                                                         */
@@ -65,6 +77,7 @@ public class MenuControll : MonoBehaviour
     public void SetMasterVolume(float volume)
     {
         audioMixer.SetFloat("masterVolume", Mathf.Log10(volume) * 20);
+        
     }
 
     /*********************************************************************************************************************************/
@@ -76,6 +89,7 @@ public class MenuControll : MonoBehaviour
     public void SetMusicVolume(float volume)
     {
         audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
+        
     }
     /*********************************************************************************************************************************/
     /*Funcion: SetSoundVolume                                                                                                        */
@@ -86,6 +100,7 @@ public class MenuControll : MonoBehaviour
     public void SetSoundVolume(float volume)
     {
         audioMixer.SetFloat("soundsVolume", Mathf.Log10(volume) * 20);
+        
     }
 
     /*********************************************************************************************************************************/
@@ -112,10 +127,15 @@ public class MenuControll : MonoBehaviour
     }
 
 
+    /*********************************************************************************************************************************/
+    /*Funcion: SetBrightness                                                                                                         */
+    /*Desarrollador: Vazquez                                                                                                         */
+    /*Parametros de entrada: value (valor del slider para aumentar o reducir el brillo)                                              */
+    /*Descripción: activa o desactiva la pantalla completa                                                                           */
+    /*********************************************************************************************************************************/
     public void SetBrightness(float value)
     {
-        brightnessPanel.color = new Color(brightnessPanel.color.r, brightnessPanel.color.g, brightnessPanel.color.b, value);
-        
+        brightnessPanel.color = new Color(brightnessPanel.color.r, brightnessPanel.color.g, brightnessPanel.color.b, value);       
     }
 
 
@@ -139,15 +159,54 @@ public class MenuControll : MonoBehaviour
     public void CloseGame()
     {
         Application.Quit();
-    } 
+    }
 
+    private void LoadSetting()
+    {
+        if (PlayerPrefs.GetInt("fullScreen") == 1)
+            fullscreenCheck.isOn = true;
+        else
+            fullscreenCheck.isOn = false;
+        SetFullScreen(fullscreenCheck.isOn);
+        qualityDropdown.value = PlayerPrefs.GetInt("quality", 0);
+        SetQuality(qualityDropdown.value);
+        resolutionDropdown.value = PlayerPrefs.GetInt("resolution", 0);
+        SetResolution(resolutionDropdown.value);
+        brightnessSlider.value = PlayerPrefs.GetFloat("Brightness", 0f);
+        brightnessPanel.color = new Color(brightnessPanel.color.r, brightnessPanel.color.g, brightnessPanel.color.b, brightnessSlider.value);
+        masterVolume.value = PlayerPrefs.GetFloat("masterVolume", 0.5f);
+        SetMasterVolume(masterVolume.value);
+        musicVolume.value = PlayerPrefs.GetFloat("musicVolume", 0.5f);
+        SetMusicVolume(musicVolume.value);
+        soundVolume.value = PlayerPrefs.GetFloat("soundVolume", 0.5f);
+        SetSoundVolume(soundVolume.value);
 
+    }
+
+    public void Deny()
+    {
+        LoadSetting();
+    }
+    public void Accept()
+    {
+        SaveSetting();
+    }
+    private void SaveSetting()
+    {
+        if (fullscreenCheck.isOn)
+            PlayerPrefs.SetInt("fullScreen", 1);
+        else
+            PlayerPrefs.SetInt("fullScreen", 0);
+        PlayerPrefs.SetInt("quality", QualitySettings.GetQualityLevel());
+        PlayerPrefs.SetInt("resolution", resolutionDropdown.value);
+        PlayerPrefs.SetFloat("Brightness", brightnessSlider.value);
+        PlayerPrefs.SetFloat("masterVolume", masterVolume.value);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume.value);
+        PlayerPrefs.SetFloat("soundVolume", soundVolume.value);
+    }
     public IEnumerator HideObject(GameObject hideImage)
     {
-        if(hideImage.name =="AlertPanel")
-            yield return new WaitForSeconds(1);
-        else
-            yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);      
         hideImage.SetActive(false);
     }
 }
